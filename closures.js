9833dcf6c5900a83ff4a8bad703c1e0a75196825
scottjason <scottleejason@gmail.com>
2014-09-03 01:42:37 -0700

    adds fetch Users closures

--- closures.js
+++ closures.js
@@ -15,6 +15,7 @@ dingDong(); // => 'Hello Scott. Nice to meet you.'
 
 //------------------------------------------------------------------
 
+// this example differs from above as the closure is not returned, it is called in the parent function (line 25)
 function getData() {
   var data = ["2", "4", "6", "8", "10"]; // the local variable data declared in getData();
   function doubleData() { // doubleData(); is the closure, the inner function that can access data
@@ -25,3 +26,35 @@ function getData() {
   doubleData();
 }
 getData();
+
+//------------------------------------------------------------------
+
+// closures can be used to maintain state and privacy
+function fetchAllUsers() {
+  var users = [{
+    firstName: "Scott",
+    lastName: "Jason",
+    email: "scott@scott.com",
+    hair: "blone",
+    hasTwoFirstNames: true
+  }, {
+    firstName: "Austin",
+    lastName: "Bourdier",
+    email: "austin@austin.com",
+    hair: "brown",
+    hasTwoFirstNames: false
+  }, {
+    firstName: "Jessica",
+    lastName: "Raynes",
+    email: "jessica@jessica.com",
+    hair: "blonde",
+    hasTwoFirstNames: false
+  }];
+  return function renderUsers() {
+    for (var i = 0; i < users.length; i++)
+      console.log(users[i]);
+  };
+}
+
+allUsers = fetchAllUsers();
+allUsers();
