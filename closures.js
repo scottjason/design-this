aff26eba68cb9125b523d2e6937b36fbfbdf7762
scottjason <scottleejason@gmail.com>
2014-09-03 02:43:36 -0700

    adds module pattern function

--- closures.js
+++ closures.js
@@ -1,3 +1,10 @@
+// -----------------------------------
+// CLOSURES
+// -----------------------------------
+
+ // closure is a special kind of object that combines two things: a function, and the
+ // environment in which that function was created.
+
 function whosThere() {
   var name = "Scott";
   return function sayHello() {
@@ -6,30 +13,16 @@ function whosThere() {
 }
 
 var dingDong = whosThere();
-// this stores the closure function itself
+// this becomes the closure sayHello();
 console.log(dingDong) // => [Function: sayHello]
 
 dingDong(); // => 'Hello Scott. Nice to meet you.'
 // while this returns the return value of the closure function
 // it invokes the closure function
 
-//------------------------------------------------------------------
-
-// this example differs from above as the closure is not returned, it is called in the parent function (line 25)
-function getData() {
-  var data = ["2", "4", "6", "8", "10"]; // the local variable data declared in getData();
-  function doubleData() { // doubleData(); is the closure, the inner function that can access data
-    for (var i = 0; i < data.length; i++) {
-      console.log(Math.floor(data[i] * 2));
-    }
-  }
-  doubleData();
-}
-getData();
 
 //------------------------------------------------------------------
 
-// closures can be used to maintain state and privacy
 function fetchAllUsers() {
   var users = [{
     firstName: "Scott",
@@ -57,4 +50,35 @@ function fetchAllUsers() {
 }
 
 allUsers = fetchAllUsers();
+// allUsers becomes the closure function renderUsers();
 allUsers();
+
+//------------------------------------------------------------------
+
+// -----------------------------------
+// LEXICAL SCOPING
+// -----------------------------------
+
+// this example differs from the above examples, getData() is called, which calls a nested function, doubleData()
+// nested functions have access to the variables defined in the parent functions.
+function getData() {
+  var data = ["2", "4", "6", "8", "10"]; // the local variable data declared in getData();
+  function doubleData() { // doubleData(); is the closure, the inner function that can access data
+    for (var i = 0; i < data.length; i++) {
+      console.log(Math.floor(data[i] * 2));
+    }
+  }
+  doubleData();
+}
+getData();
+
+// doubleData(); is defined in getData(); only accessible within the body of this parent function
+
+//------------------------------------------------------------------
+
+// Private methods are used for restricting access to code, managing your global namespace, keeping non-essential
+// methods from cluttering up the public interface to your code.
+
+// -----------------------------------
+// PRIVATE METHODS WITH CLOSURES
+// -----------------------------------
---
+++ modulePattern.js
@@ -0,0 +1,35 @@
+// THE MODULE PATTERN
+
+// PRIVATE METHODS WITH CLOSURES
+// USING CLOSURES TO PROVIDE PUBLIC FUNCTIONS ACCESS PRIVATE FUNCTIONS AND VARIABLES.
+
+
+// this creates a single environment shared by four enviorments
+
+var wordBuilder = (function() {
+  var aPrivateWord = "SC";
+
+  function addChar( character ) {
+    aPrivateWord = aPrivateWord.concat( character )
+  }
+  return {
+    addFirst: function() {
+      addChar( "O" );
+    },
+    addSecond: function() {
+      addChar( "T" );
+    },
+    addThird: function() {
+      addChar( "T" );
+    },
+    result: function() {
+      return aPrivateWord
+    }
+  };
+})();
+
+
+wordBuilder.addFirst();
+wordBuilder.addSecond();
+wordBuilder.addThird();
+wordBuilder.result() // => 'SCOTT'
