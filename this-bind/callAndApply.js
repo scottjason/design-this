81ed97d7f9bb2bf30627ab8fb2672937a6d6ae4b
scottjason <scottleejason@gmail.com>
2014-09-03 22:07:14 -0700

    completes call and apply

--- this-bind/callAndApply.js
+++ this-bind/callAndApply.js
@@ -26,7 +26,7 @@ getCoolness.call(reality, "now getting status from the object we've explicitly p
 // And via apply() with an array.
 
 var status = "way cool";
-var reality = { status: "even cooler" }
+var reality = { status: "even cooler" } // not used
 function getCoolness( message ){
   console.log( message )
   console.log( this.status ); // => call-stite
@@ -40,3 +40,30 @@ getCoolness.call(reality, ["my status was just created via the apply(..) method.
 // way cool
 // [ 'my status was just created via the apply(..) method.I am the apply' ]
 // even cooler
+
+//------------------------------------------------------------------
+
+// we can use this array to filter what we want, and pass through to another function like this:
+
+var userLocation = { currentGeo: "San Francisco" };
+
+function readyToGetLocation( message1, message2 )
+{
+    console.log( message1 + ( this.currentGeo ) + message2 );
+}
+
+function filterForPassThrough( object, callback )
+{
+ // note: arguments is a keyword in JS representing the elements of an array
+ // arguments[0] == the object && arguments[1] == the callback
+    var filteredArr = [];
+    for(var i = 2; i < arguments.length; i++)
+    {
+        filteredArr.push(arguments[i]);
+    }
+
+    callback.apply( object, filteredArr ); // passing in the same object (same "this")
+}
+
+filterForPassThrough(userLocation, readyToGetLocation, "Scott's current location is ", " and right now he's on the computer.");
+// four arguments being passed in
