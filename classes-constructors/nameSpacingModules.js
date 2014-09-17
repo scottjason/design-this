c0cd699ce7d39bd6ce7e57829a81d65bd1b5e6c3
scottjason <scottleejason@gmail.com>
2014-09-17 16:23:41 -0700

    adds function to namespaced module pattern

--- classes-constructors/nameSpacingModules.js
+++ classes-constructors/nameSpacingModules.js
@@ -9,13 +9,23 @@ DesignThis.UserModule = (function() {
         return "Welcome " + name + ", " + message;
     };
 
+    var sendGoodbye = function( name, message ) {
+        return "Sorry to see you go, " + name + "." + " " + message;
+    }
+
     return {
         // welcomeUser is a public function
         welcomeUser: function( name, message ) {
             return sendGreeting( name, message );
+        },
+        goodbyeUser: function( name, message ) {
+            return sendGoodbye( name, message );
         }
     };
 })();
 
 DesignThis.UserModule.welcomeUser("Scott", "glad to have you aboard!");
 // => 'Welcome Scott, glad to have you aboard!'
+
+DesignThis.UserModule.goodbyeUser('Scott', 'Please let us know if there is anything we can do to improve you experience.');
+//=> 'Sorry to see you go, Scott. Please let us know if there is anything we can do to improve you experience.'
