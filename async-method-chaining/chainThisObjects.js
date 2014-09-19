1b2bb92ad0673220a0d1d3e57582341ee2337799
scottjason <scottleejason@gmail.com>
2014-09-18 23:05:27 -0700

    adds targetUser function in object

--- async-method-chaining/chainThisObjects.js
+++ async-method-chaining/chainThisObjects.js
@@ -1,23 +1,41 @@
- // lets create an object, just like what database would store
+ // lets create an object, just like a what database would store
  var users = [{
      id: 102,
      firstName: "scott",
      lastName: "jason",
-     handle: "scott@jason.com"
- }, {
-     id: 214,
+     email: "scott@jason.com"
+ },
+     id: 322,
      firstName: "austin",
      lastName: "bourdier",
-     handle: "austin@bourdier.com"
+     email: "austin@bourdier.com"
  }];
 
 
+ // lets create a helper function that will properly display our users name in the browser
+ function capitalize( str ) {
+     return str.replace(/^./, function( match ) {
+         return match.toUpperCase();
+     });
+ }
 
-// lets create a helper function that will properly display our users names in the browser
-function capitalize( str ) {
-   return str.replace(/^./, function ( match ) {
-        return match.toUpperCase();
-    });
-}
+ // lets create our object with chainable methods
+ var userModule = {
+    var targetUser; // hoisting the targetUser
+
+      filterUser: function( email ) {
+        for( var i = 0; i < users.length; i++ ){
+          if( users[i] == users.email ) {
+            this.targetUser = users[i];
+            break;
+          }
+        }
+      },
+     formatUser: function( name ) {
 
 
+     },
+     greetUser: function() {
+
+     }
+ }
